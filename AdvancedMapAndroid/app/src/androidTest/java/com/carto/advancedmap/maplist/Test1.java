51670c5bae9a8a6884f884bdb097c19dab5f444f
Aare Undo <aare@carto.com>
2016-10-06 12:13:07 +0300

    AdvancedMap: test update

--- AdvancedMapAndroid/app/src/androidTest/java/com/carto/advancedmap/maplist/Test1.java
+++ AdvancedMapAndroid/app/src/androidTest/java/com/carto/advancedmap/maplist/Test1.java
@@ -1,7 +1,9 @@
 package com.carto.advancedmap.maplist;
 
 
+import android.support.test.espresso.Espresso;
 import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
@@ -16,12 +18,19 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Map;
+
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.*;
 import static android.support.test.espresso.assertion.ViewAssertions.*;
 import static android.support.test.espresso.matcher.ViewMatchers.*;
 import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.collection.IsMapContaining.hasEntry;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -36,14 +45,22 @@ public class Test1 {
         int mapCount = 21;
 
         for (Integer i = 0; i < mapCount; i++) {
-            ViewInteraction linearLayout = onView(getMatcher(i));
-            linearLayout.perform(click());
-            pressBack();
+
+            // TODO Check if !visible -> scroll to visible
+            try {
+                onView(getMatcher(i)).perform(click());
+                pressBack();
+            } catch (Exception e) {
+                return;
+            }
         }
     }
 
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
+    static Matcher<View> getMatcher(Integer i) {
+        return allOf(childAtPosition(allOf(withId(android.R.id.list), withParent(withId(android.R.id.content))), i), isDisplayed());
+    }
+
+    private static Matcher<View> childAtPosition(final Matcher<View> parentMatcher, final int position) {
 
         return new TypeSafeMatcher<View>() {
             @Override
@@ -59,9 +76,5 @@ public class Test1 {
                         && view.equals(((ViewGroup) parent).getChildAt(position));
             }
         };
-    }
-
-    static Matcher<View> getMatcher(Integer i) {
-        return allOf(childAtPosition(allOf(withId(android.R.id.list), withParent(withId(android.R.id.content))), i), isDisplayed());
     }
 }
