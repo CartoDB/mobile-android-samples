56f490e10e91c0440683c13da358db706c3b18b4
Aare Undo <aare@carto.com>
2016-10-17 18:04:32 +0300

    CartoMap: new api use in SQlServiceActivity

--- CartoMapAndroid/app/src/main/java/com/carto/cartomap/android/sections/sqlapi/SQLServiceActivity.java
+++ CartoMapAndroid/app/src/main/java/com/carto/cartomap/android/sections/sqlapi/SQLServiceActivity.java
@@ -2,11 +2,23 @@ package com.carto.cartomap.android.sections.sqlapi;
 
 import android.os.Bundle;
 
+import com.carto.cartomap.android._old.datasource.CartoDBSQLDataSource;
 import com.carto.cartomap.android.sections.BaseMapActivity;
 import com.carto.cartomap.android.util.Description;
-import com.carto.core.Variant;
+import com.carto.datasources.LocalVectorDataSource;
+import com.carto.datasources.VectorDataSource;
+import com.carto.datasources.components.VectorData;
+import com.carto.geometry.FeatureCollection;
+import com.carto.geometry.LineGeometry;
+import com.carto.geometry.PointGeometry;
+import com.carto.graphics.Color;
+import com.carto.layers.VectorLayer;
+import com.carto.renderers.components.CullState;
 import com.carto.services.CartoSQLService;
-import com.carto.utils.Log;
+import com.carto.styles.PointStyle;
+import com.carto.styles.PointStyleBuilder;
+import com.carto.vectorelements.Point;
+import com.carto.vectorelements.VectorElementVector;
 
 import java.io.IOException;
 
@@ -17,20 +29,20 @@ import java.io.IOException;
 @Description(value = "Displays cities on the map via SQL query")
 public class SQLServiceActivity extends BaseMapActivity {
 
-//    static final String query = "SELECT * FROM cities15000 WHERE population > 100000";
-    static final String query = "SELECT * FROM cities15000";
+    static final String query = "SELECT * FROM cities15000 WHERE population > 100000";
+//    static final String query = "SELECT * FROM cities15000";
 
-    Variant result = null;
+    FeatureCollection features = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        final LocalVectorDataSource source = new LocalVectorDataSource(baseProjection);
+        final VectorLayer layer = new VectorLayer(source);
+        mapView.getLayers().add(layer);
+
         final CartoSQLService service = new CartoSQLService();
-
-        // Sample url:
-        // https://cartomobile-team.carto.com/u/nutiteq/viz/f1407ed4-84b8-11e6-96bc-0ee66e2c9693/public_map
-
         service.setUsername("nutiteq");
 
         // Be sure to make network queries on another thread
@@ -38,21 +50,35 @@ public class SQLServiceActivity extends BaseMapActivity {
             @Override
             public void run() {
                 try {
-                    result = service.queryData(query);
+                    features = service.queryFeatures(query);
+
+                    for (int i = 0; i < features.getFeatureCount(); i++) {
+
+                        // This data set features point geometry,
+                        // however, it can also be LineGeometry or PolygonGeometry
+
+                        PointGeometry geometry = (PointGeometry)features.getFeature(i).getGeometry();
+                        source.add(new Point(geometry, getPointStyle()));
+                        System.out.println("Element: " + geometry);
+                    }
+
+                    System.out.println("Total: " + features.getFeatureCount());
+
                 } catch (IOException e) {
                     e.printStackTrace();
-                }
-
-                Variant rows = result.getObjectElement("rows");
-
-                for (int i = 0; i < rows.getArraySize(); i++) {
-
-                    Variant row = rows.getArrayElement(i);
-//                    System.out.println("Geom: " + row.getObjectElement("the_geom"));
                 }
             }
         });
 
         thread.start();
+    }
+
+    PointStyle getPointStyle() {
+
+        PointStyleBuilder pointStyleBuilder = new PointStyleBuilder();
+        pointStyleBuilder.setColor(new Color(android.graphics.Color.CYAN));
+        pointStyleBuilder.setSize(10);
+
+        return pointStyleBuilder.buildStyle();
     }
 }
