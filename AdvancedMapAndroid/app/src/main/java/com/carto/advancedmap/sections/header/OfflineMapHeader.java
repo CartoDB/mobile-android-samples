e0893ba72185f909f12850898580aa5292a491ab
Aare Undo <aare@carto.com>
2017-04-17 16:15:23 +0300

    Advanced: different routing bbox download; title fix

--- AdvancedMapAndroid/app/src/main/java/com/carto/advancedmap/sections/header/OfflineMapHeader.java
+++ AdvancedMapAndroid/app/src/main/java/com/carto/advancedmap/sections/header/OfflineMapHeader.java
@@ -8,6 +8,6 @@ import com.carto.advancedmap.list.ActivityData;
  * Created by aareundo on 14/10/16.
  */
 
-@ActivityData(name = "Other", description = "")
+@ActivityData(name = "Offline", description = "")
 public class OfflineMapHeader extends Activity {
 }
