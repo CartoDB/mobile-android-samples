b8c78f3e89c92049534218629c65813913496414
Aare Undo <aare@carto.com>
2017-07-12 13:12:51 +0300

    AdvancedMap.Kotlin: location permission logic

--- AdvancedMapKotlin/app/src/main/java/com/carto/advanced/kotlin/sections/base/BaseActivity.kt
+++ AdvancedMapKotlin/app/src/main/java/com/carto/advanced/kotlin/sections/base/BaseActivity.kt
@@ -1,5 +1,6 @@
 package com.carto.advanced.kotlin.sections.base
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
@@ -7,6 +8,9 @@ import android.view.KeyEvent
 import android.view.MenuItem
 import android.widget.Toast
 import com.carto.advanced.kotlin.main.MainActivity
+import android.support.v4.app.ActivityCompat
+
+
 
 /**
  * Created by aareundo on 03/07/2017.
@@ -32,10 +36,35 @@ open class BaseActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
+    fun requestPermission(permission: String) {
+        ActivityCompat.requestPermissions(this, arrayOf(permission), 1)
+    }
+
+    fun requestPermissions(permissions: Array<String>) {
+        ActivityCompat.requestPermissions(this, permissions, 1)
+    }
+
+    open fun onPermissionsGranted(granted: Boolean) {
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            1 -> {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    onPermissionsGranted(true)
+                } else {
+                    onPermissionsGranted(false)
+                }
+                return
+            }
+        }
+    }
+
     fun alert(message: String) {
         runOnUiThread {
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
         }
-
     }
 }
--- AdvancedMapKotlin/app/src/main/java/com/carto/advanced/kotlin/sections/gpslocation/GPSLocationActivity.kt
+++ AdvancedMapKotlin/app/src/main/java/com/carto/advanced/kotlin/sections/gpslocation/GPSLocationActivity.kt
@@ -1,17 +1,27 @@
 package com.carto.advanced.kotlin.sections.gpslocation
 
+import android.Manifest
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.view.View
 import com.carto.advanced.kotlin.sections.base.BaseActivity
 
 class GPSLocationActivity : BaseActivity() {
 
     var contentView: GPSLocationView? = null
 
+    var manager: LocationManager? = null
+    var listener: LocationListener? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         contentView = GPSLocationView(this)
         setContentView(contentView)
+
+        val permission = Manifest.permission.ACCESS_FINE_LOCATION
+        requestPermission(permission)
     }
 
     override fun onResume() {
@@ -22,5 +32,17 @@ class GPSLocationActivity : BaseActivity() {
     override fun onPause() {
         super.onPause()
         contentView?.removeListeners()
+    }
+
+    override fun onPermissionsGranted(granted: Boolean) {
+
+        if (granted) {
+
+        } else {
+            val text = "Fine, so we won't track your location"
+            contentView?.topBanner?.setText(text)
+            contentView?.layoutSubviews()
+            contentView?.switch?.visibility = View.GONE
+        }
     }
 }
