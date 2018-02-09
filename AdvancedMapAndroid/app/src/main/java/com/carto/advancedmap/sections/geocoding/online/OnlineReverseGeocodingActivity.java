5f9f92e92320fc41f6c859057f2a653f8f237f78
Aare Undo <aare@carto.com>
2018-02-09 14:09:36 +0200

    Advanced.Java: removd mapzen references

--- AdvancedMapAndroid/app/src/main/java/com/carto/advancedmap/sections/geocoding/online/OnlineReverseGeocodingActivity.java
+++ AdvancedMapAndroid/app/src/main/java/com/carto/advancedmap/sections/geocoding/online/OnlineReverseGeocodingActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.carto.advancedmap.main.ActivityData;
 import com.carto.advancedmap.sections.geocoding.base.ReverseGeocodingBaseActivity;
 import com.carto.advancedmap.utils.Sources;
+import com.carto.geocoding.MapBoxOnlineReverseGeocodingService;
 import com.carto.geocoding.PeliasOnlineReverseGeocodingService;
 
 /**
@@ -22,7 +23,7 @@ public class OnlineReverseGeocodingActivity extends ReverseGeocodingBaseActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        service = new PeliasOnlineReverseGeocodingService(Sources.API_KEY);
+        service = new MapBoxOnlineReverseGeocodingService(Sources.MAPBOX_TOKEN);
 
         contentView.removeButton(contentView.downloadButton);
     }
--- AdvancedMapAndroid/app/src/main/java/com/carto/advancedmap/utils/Sources.java
+++ AdvancedMapAndroid/app/src/main/java/com/carto/advancedmap/utils/Sources.java
@@ -7,8 +7,6 @@ package com.carto.advancedmap.utils;
 public class Sources {
 
     public static final String CARTO_VECTOR = "carto.streets";
-    public static final String MAPZEN = "mapzen.osm";
-    public static final String CARTO_RASTER = "carto.osm";
 
     public static final String OFFLINE_ROUTING = CARTO_VECTOR;
 
@@ -17,5 +15,5 @@ public class Sources {
     public static final String GEOCODING_TAG = "geocoding:";
     public static final String OFFLINE_GEOCODING = CARTO_VECTOR;
 
-    public static final String API_KEY = "mapzen-e2gmwsC";
+    public static final String MAPBOX_TOKEN = "pk.eyJ1IjoiamFha2wiLCJhIjoiR3FYNFJrRSJ9.n8PoOPy9tvnULFJ8h67pIA";
 }
